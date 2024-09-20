1029e93f8a16bc5ec17d19e5f519c7e14e542870
Carlos Ferreira <161725212+CarlosHenriqueBatistaFerreira@users.noreply.github.com>
2024-09-20 12:13:22 -0300

    20/09/24

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Oi ");
+console.log("Oi Joao");
+
+var titulo = document.querySelector("h1");
+titulo.textContent = "Carlos Nutrição";
+
+var trPaulo = document.querySelector("#paulo");
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
