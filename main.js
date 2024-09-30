91e96231f96a382d4c69c0562806b39ec1807d47
Carlos Ferreira <161725212+CarlosHenriqueBatistaFerreira@users.noreply.github.com>
2024-09-30 10:41:03 -0300

    30/09/2024

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-var ttiulo = document.querySelector(".titulo");
-titulo.textContent = "Carlos Nutrição";
+var pacientes = document.querySelectorAll('.paciente');
 
-var paciente =  document.querySelector("#primeiro-paciente");
+for (var i = 0; i < 5; i++){
+  console.log(pacientes[i]);
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+  var tdPeso = pacientes[i].querySelector('.info-peso');
+  var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdAltura = pacientes[i].querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+
+  var tdIMC = pacientes[i].querySelector('.info-imc');
+
+  var pesoEnValido = true;
+  var alturaEnValida = true;
+
+  if(peso <= 0 || peso >= 600){
+    tdIMC.textContent = "Peso inválido ";
+    pesoEnValido = false;
+    pacientes[i].style.color = "white";
+  }
+
+  if(altura <= 0 || altura > 2.80){
+    tdIMC.textContent = "Altura inválido ";
+    alturaEnValida = false;
+  }
+
+  if (pesoEnValido && alturaEnValida){
+    var imc = peso / (altura*altura);
+    tdIMC.textContent = imc;
+  }
+}
